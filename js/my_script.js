07bd62acd703aa05c762ec069ba4c388045d4192
sanuv9683 <sanuv9683@gmail.com>
2024-10-28 00:09:23 +0000

    fixed the styles issues

---
+++ js/my_script.js
@@ -0,0 +1,11 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: "bottom-center",
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+    }
+});
