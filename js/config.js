4afa6270a6b600740c06b23141e78ae79a6fcb1c
sanuv9683 <sanuv9683@gmail.com>
2024-11-25 01:47:56 +0000

    content Updated

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCZh-clauS34WlvfBQf8pZqyH2RbnsxgO8",
+    authDomain: "freespace-efa32.firebaseapp.com",
+    databaseURL: "https://freespace-efa32-default-rtdb.firebaseio.com",
+    projectId: "freespace-efa32",
+    storageBucket: "freespace-efa32.appspot.com",
+    messagingSenderId: "933704401359",
+    appId: "1:933704401359:web:7390a1d897f3d554ed12ba",
+    measurementId: "G-C671C1TRKM"
+};
